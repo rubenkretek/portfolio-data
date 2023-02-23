8adf79b6b051d92471d43bcf53404e143b5a8a31
rubenkretek <rubenkretek@outlook.com>
2023-02-23 16:33:34 +0000

    Sanity setup

---
+++ sanity.cli.js
@@ -0,0 +1,8 @@
+import {defineCliConfig} from 'sanity/cli'
+
+export default defineCliConfig({
+  api: {
+    projectId: 'gju3hygu',
+    dataset: 'production'
+  }
+})
---
+++ schemas/index.js
@@ -0,0 +1,6 @@
+import blockContent from './blockContent'
+import category from './category'
+import project from './project'
+import skill from './skill'
+
+export const schemaTypes = [project, skill, category, blockContent]
